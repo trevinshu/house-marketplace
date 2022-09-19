11927c9aba87695dd8ba2b5b8255f01da1c30339
trevinshu <trevinshu2008@gmail.com>
2022-09-19 00:56:58 -0600

    finished section 15: Get & Create Listings

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 import Spinner from '../components/Spinner';
 import ListingItem from '../components/ListingItem';
 
-function Offers() {
+function Category() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -19,7 +19,7 @@ function Offers() {
         const listingsRef = collection(db, 'listings');
 
         //Create a query
-        const q = query(listingsRef, where('offer', '==', true), orderBy('timestamp', 'desc'), limit(10));
+        const q = query(listingsRef, where('type', '==', params.categoryName), orderBy('timestamp', 'desc'), limit(10));
 
         //Execute query
         const querySnap = await getDocs(q);
@@ -40,11 +40,11 @@ function Offers() {
     };
 
     fetchListings();
-  }, []);
+  }, [params.categoryName]);
   return (
     <div className="category">
       <header>
-        <p className="pageHeader">Offers</p>
+        <p className="pageHeader">{params.categoryName === 'rent' ? 'Places for rent' : 'Places for sale'}</p>
       </header>
       {loading ? (
         <Spinner />
@@ -59,10 +59,10 @@ function Offers() {
           </main>
         </>
       ) : (
-        <p>There are no current offers {params.categoryName}</p>
+        <p>No listings for {params.categoryName}</p>
       )}
     </div>
   );
 }
 
-export default Offers;
+export default Category;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { getAuth, updateProfile } from 'firebase/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import { db } from '../firebase.config';
 import { updateDoc, doc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
+import homeIcon from '../assets/svg/homeIcon.svg';
 
 function Profile() {
   const auth = getAuth();
@@ -75,6 +77,11 @@ function Profile() {
             <input type="text" id="email" className={!changeDetails ? 'profileEmail' : 'profileEmailActive'} disabled={!changeDetails} value={email} onChange={onChange} />
           </form>
         </div>
+        <Link to="/create-listing" className="createListing">
+          <img src={homeIcon} alt="home" />
+          <p>Sell or rent your home</p>
+          <img src={arrowRight} alt="arrowRight" />
+        </Link>
       </main>
     </div>
   );
