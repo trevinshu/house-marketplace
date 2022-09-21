70cd031484c2ed9fcc45df42cbc82c1abb24dac3
trevinshu <trevinshu2008@gmail.com>
2022-09-20 21:31:46 -0600

    Revert "finished section 16: single listings, map, slider & edit"
    This reverts commit 493906fc101614e505be9b29f47fc92087e1fedf.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { getAuth, updateProfile } from 'firebase/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import { db } from '../firebase.config';
-import { updateDoc, doc, collection, getDocs, query, where, orderBy, deleteDoc } from 'firebase/firestore';
+import { updateDoc, doc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
 import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
 import homeIcon from '../assets/svg/homeIcon.svg';
-import ListingItem from '../components/ListingItem';
 
 function Profile() {
   const auth = getAuth();
-  const [loading, setLoading] = useState(true);
-  const [listings, setListings] = useState(null);
   const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -21,30 +18,6 @@ function Profile() {
   const { name, email } = formData;
 
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchUserListings = async () => {
-      const listingsRef = collection(db, 'listings');
-
-      const q = query(listingsRef, where('userRef', '==', auth.currentUser.uid), orderBy('timestamp', 'desc'));
-
-      const querySnap = await getDocs(q);
-
-      let listings = [];
-
-      querySnap.forEach((doc) => {
-        return listings.push({
-          id: doc.id,
-          data: doc.data(),
-        });
-      });
-
-      setListings(listings);
-      setLoading(true);
-    };
-
-    fetchUserListings();
-  }, [auth.currentUser.uid]);
 
   const onLogout = () => {
     auth.signOut();
@@ -77,9 +50,6 @@ function Profile() {
     }));
   };
 
-  const onDelete = () => [];
-
-  const onEdit = () => [];
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -112,17 +82,6 @@ function Profile() {
           <p>Sell or rent your home</p>
           <img src={arrowRight} alt="arrowRight" />
         </Link>
-
-        {!loading && listings?.length > 0 && (
-          <>
-            <p className="listingText">Your Listings</p>
-            <ul className="listingsList">
-              {listings.map((listing) => (
-                <ListingItem key={listing.id} listing={listing.data} id={listing.id} onDelete={() => onDelete(listing.id)} onEdit={() => onEdit(listing.id)} />
-              ))}
-            </ul>
-          </>
-        )}
       </main>
     </div>
   );
